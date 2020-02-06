e8ecf7bc545d34636289a95dc3186ae0b6631af9
Monika Rakoczy <monika@portable6317.ipv6.lip6.fr>
2020-02-06 17:45:15 +0100

    lda algorithm + not finished tf-idf with data divided in weeks

--- run-tfidf.R
+++ run-tfidf.R
@@ -8,11 +8,12 @@ library(stringr)
 json_data <- fromJSON(txt = "uk_parliament_climatechange.json")$speeches
 
 discussions_words <- json_data %>% 
-  unnest_tokens(word, text) %>%
+  unnest_tokens(word, text, to_lower = TRUE) %>%
   filter(!str_detect(word, "^[0-9]*$")) %>%
   anti_join(stop_words) %>% #removing stop words
 #  mutate(word = SnowballC::wordStem(word)) %>% # tokens -> roots (Porter stemming algorithm) 
   count(discussion_title, word, sort = TRUE)
+
 
 discussions <- discussions_words %>%
   bind_tf_idf(word, discussion_title, n) %>%
@@ -20,7 +21,7 @@ discussions <- discussions_words %>%
 
 discussions <- discussions %>%  arrange(desc(discussion_title, tf_idf))
 
-number_of_top<-1 #number of top words
+number_of_top<-15 #number of top words
 discussions_top <- discussions %>% 
   group_by(discussion_title) %>% 
   arrange(discussion_title, desc(tf_idf)) %>% 
@@ -33,7 +34,7 @@ discussions_top <- discussions %>%
 discussions_top <-discussions_top %>% 
   inner_join(json_data%>% select(date, discussion_title) %>% distinct(), by = "discussion_title")
 
-json_data$id
+write.csv2(discussions_top, file = "/home/monika/Documents/UMPC/Timeline/idf-trial/top15_topics_per_discussion_title.csv", quote=FALSE, row.names = FALSE)
 
 discussions_count <- json_data %>% 
   count(discussion_title, date)
@@ -49,3 +50,8 @@ ggplot(discussions_count, aes(x=date)) +
 
 
 
+
+################### dataset info
+# unique(json_data[c("date","discussion_title")]) ##same discusssions on different days 
+# length(unique(json_data$date)) #344
+# length(unique(json_data$discussion_title)) #560
---
+++ run_LDA.R
@@ -0,0 +1,28 @@
+library(jsonlite)
+# library(tidytext)
+# library(dplyr)
+#library(forcats)
+library(ggplot2)
+# library(stringr)
+library(quanteda)
+library(topicmodels)
+library(lubridate)
+
+
+
+json_data <- fromJSON(txt = "uk_parliament_climatechange.json")$speeches
+
+
+dfm_discussion_words <- json_data$text %>% 
+  dfm(remove_punct = TRUE, remove =stopwords('en')) %>% 
+  dfm_remove(c('^[0-9]*$')) %>%
+  dfm_trim(min_termfreq = 0.95, termfreq_type = "quantile", 
+         max_docfreq = 0.1, docfreq_type = "prop")
+
+dfm_discussion_words <- dfm_discussion_words[ntoken(dfm_discussion_words) > 0,]
+
+
+dtm <- convert(dfm_discussion_words, to = "topicmodels")
+lda <- topicmodels::LDA(dtm, k = 10)
+
+terms(lda, 10)
