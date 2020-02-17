f80bef5b2aece26804e942e2dd7f974a825cacbf
Monika Rakoczy <monika@portable6317.ipv6.lip6.fr>
2020-02-17 15:47:55 +0100

    plots and script with investigation article-section

--- guardian_distribution-plots.R
+++ guardian_distribution-plots.R
@@ -29,3 +29,54 @@ colnames(grouped)<-c("art_date_published","art_count")
 p <- ggplot(grouped, aes(x = art_date_published, y = art_count)) + geom_col() 
 p + theme(axis.text.x = element_text(angle = 45)) + xlab("Date") + ylab("# of articles")
 ggsave(paste(getwd(),"/plots/articles-per-date.pdf", sep=''))
+
+
+grouped <- data %>% group_by(article_section) %>%   
+  count() 
+
+colnames(grouped)<-c("art_section","art_count")
+
+p <- ggplot(grouped, aes(x = art_section, y = art_count)) + geom_col() 
+p + theme(axis.text.x = element_text(angle = 90)) + xlab("Section") + ylab("# of articles")
+ggsave(paste(getwd(),"/plots/articles-per-section.pdf", sep=''))
+
+#grouped_limited %>% filter(art_count== min(grouped_limited$art_count))
+
+grouped_limited<-grouped[!(grouped$art_section=="Environment"),]
+
+p <- ggplot(grouped_limited, aes(x = art_section, y = art_count)) + geom_col() 
+p + 
+  theme(axis.text.x = element_text(angle = 90)) +
+  xlab("Section") + ylab("# of articles")
+ggsave(paste(getwd(),"/plots/articles-per-section-noenv.pdf", sep=''))
+
+
+
+
+distribution_article_section <- function(data, sectionName, save=FALSE){
+  print(class(sectionName))
+  section_data <- data[data$article_section==sectionName,]
+  section_data <- section_data %>% 
+    count(format(date,'%y-%m-%d')) 
+  colnames(section_data)<-c("art_date_published","art_count")
+  
+  p <- ggplot(section_data, aes(x = art_date_published, y = art_count)) + geom_col() 
+  p + theme(axis.text.x = element_text(angle = 45)) + xlab("Date") + ylab("# of articles") +ggtitle(paste("Section:", sectionName))
+  
+  # if(save==TRUE){
+  #   ggsave(paste(getwd(),"/plots/section",sectionName,".pdf", sep=''), plot = last_plot())
+  # }
+}
+
+
+#####Articles selection
+##min number of articles: Children's Books (1)
+##max number of articles: Environment --> 1 from max day
+##max number of articles (2nd): Opiniton
+##australia news
+#music
+selected_articles<-data[data$article_section=="Children's books",]
+
+
+
+###section_data%>% filter(art_count== max(section_data$art_count))
